32df92310ada8e57b246cee92ee50033cb060f30
manuel <mn07@gmx.at>
2024-04-11 21:17:31 +0200

    basics finish

--- src/test/java/org/fogl/automat/TestAutomat.java
+++ src/test/java/org/fogl/automat/TestAutomat.java
@@ -1,28 +1,56 @@
 package org.fogl.automat;
 
 
+import org.fogl.FileReader;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 
 public class TestAutomat {
 
-    private final DEAAutomat deaAutomat = new DEAAutomat();
-
-
     @Test
-    public void checkAutomatComplete() {
+    public void checkAutomatTrue() {
 
-        String testInput = "ab";
+        String testInput = "1+1=2";
         boolean trueIfEndPointReached = DEAAutomat.DEA(testInput);
         Assertions.assertTrue(trueIfEndPointReached);
     }
 
     @Test
-    public void checkAutomatUnfinished() {
+    public void checkAutomatFalse() {
 
-        String testInput = "abd";
+        String testInput = "1-1";
         boolean falseIfEndpointNotReached = DEAAutomat.DEA(testInput);
         Assertions.assertFalse(falseIfEndpointNotReached);
+    }
+
+    @Test
+    public void checkAutomatA() {
+
+        String testInput = "a*b=c";
+        boolean falseIfEndpointNotReached = DEAAutomat.DEA(testInput);
+        Assertions.assertFalse(falseIfEndpointNotReached);
+    }
+
+    @Test
+    public void checkAutomatB() {
+
+        String testInput = "(a*b=c)";
+        boolean falseIfEndpointNotReached = DEAAutomat.DEA(testInput);
+        Assertions.assertFalse(falseIfEndpointNotReached);
+    }
+
+    @Test
+    public void readFile() throws IOException {
+
+        Path path = Paths.get("src", "test", "resources", "testfile.txt");
+        FileReader fileReader = new FileReader();
+        fileReader.readFile(path);
+        String[] testFile = fileReader.getAllLines();
+        Assertions.assertTrue(DEAAutomat.DEA(testFile[0]));
+        Assertions.assertFalse(DEAAutomat.DEA(testFile[1]));
     }
 }
